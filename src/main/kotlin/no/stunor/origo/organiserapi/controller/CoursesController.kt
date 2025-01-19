7c7b895ec9ad6e28eea420f4fe1345d7a4aa6a7c
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2025-01-19 21:26:07 +0100

    Add logging to course-controller

--- src/main/kotlin/no/stunor/origo/organiserapi/controller/CoursesController.kt
+++ src/main/kotlin/no/stunor/origo/organiserapi/controller/CoursesController.kt
@@ -2,11 +2,13 @@ package no.stunor.origo.organiserapi.controller
 
 import no.stunor.origo.organiserapi.services.CourseService
 import org.iof.CourseData
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 internal class CoursesController {
+    private val log = LoggerFactory.getLogger(this.javaClass)
 
     @Autowired
     private lateinit var courseService: CourseService
@@ -18,6 +20,9 @@ internal class CoursesController {
         @PathVariable(value = "raceId") raceId: String,
         @RequestBody(required = true) courseData: CourseData
     ) {
+        log.info("Saving course for eventorId: $eventorId, eventId: $eventId, raceId: $raceId")
+        log.info("Course data: $courseData")
+
         courseService.saveCourse(eventorId, eventId, raceId, courseData)
     }
 
